bdfaac5d45aef5edf90f7756cac10e79512443ff
Christopher <chrisbmineiro@gmail.com>
2024-08-10 16:24:34 -0300

    resolvendo desafios propostos e adicionando medidas de tratamento para respostas invalidas

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using DesafioFundamentos.Models;
+
+// Coloca o encoding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+                  "Digite o preço inicial:");
+while (!decimal.TryParse(Console.ReadLine(), out precoInicial))
+{
+    Console.WriteLine("Entrada inválida. Por favor, insira um valor decimal para o preço inicial:");
+}
+
+Console.WriteLine("Agora digite o preço por hora:");
+while (!decimal.TryParse(Console.ReadLine(), out precoPorHora))
+{
+    Console.WriteLine("Entrada inválida. Por favor, insira um valor decimal para o preço por hora:");
+}
+
+// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+bool exibirMenu = true;
+
+// Realiza o loop do menu
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    string? opcaoMenu = Console.ReadLine();
+    if (!string.IsNullOrEmpty(opcaoMenu))
+    {
+        switch (opcaoMenu)
+        {
+            case "1":
+                es.AdicionarVeiculo();
+                break;
+            case "2":
+                es.RemoverVeiculo();
+                break;
+            case "3":
+                es.ListarVeiculos();
+                break;
+            case "4":
+                exibirMenu = false;
+                break;
+            default:
+                Console.WriteLine("Opção inválida");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Opção inválida");
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa se encerrou");
